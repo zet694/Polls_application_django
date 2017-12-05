87725b64ffab9b0bc2c92c83e9ad37e179301096
zet694 <zzeett694@yandex.ru>
2017-12-05 23:07:40 +0300

    add admin panel fields

--- Polls/admin.py
+++ Polls/admin.py
@@ -1,6 +1,19 @@
 from django.contrib import admin
-from .models import Question,Choice
+
+from .models import Choice, Question
 
 
-admin.site.register(Question)
-admin.site.register(Choice)
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 3
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {'fields': ['quest_text']}),
+        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+    ]
+    inlines = [ChoiceInline]
+
+
+admin.site.register(Question, QuestionAdmin)
